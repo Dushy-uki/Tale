570efb7bb766376ca6ffc1566663a8aefbea3092
Dushy-uki <dushyd81@gmail.com>
2025-04-22 15:08:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Heading from "./assets/Components/Header"
+import Section from "./assets/Components/Section"
+import Footer from "./assets/Components/Footer"
+function App() {
+ 
+
+  return (
+    <>
+    <Heading/>
+    <Section/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
